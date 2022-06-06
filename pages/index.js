50bfbfcb8c4a6557d85cd70709558a2a678cbb61
Nittin Shankar <nittin.shankar@gmail.com>
2022-06-06 14:10:33 +0530

    Fetch data from markdown files and also create API routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import { getSortedPostsData } from '../lib/posts';
+
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +28,23 @@ export default function Home() {
           This is a sample website built by following this {' '}
           <a href="https://nextjs.org/learn">Next.js tutorial</a>.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
